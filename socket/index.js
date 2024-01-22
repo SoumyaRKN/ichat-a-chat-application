5161f46b887c597222c23595f2c5644019a16530
SoumyaRKN <110736827+SoumyaRKN@users.noreply.github.com>
2024-01-22 15:21:42 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://127.0.0.1:3000',
+        origin: '*',
         methods: ['GET', 'POST'],
     },
 });
